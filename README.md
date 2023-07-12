0b969565ef07dae0d65af6debadce18f4468d1ae
Soichiro Yoshimura <yoshimura@soichiro.org>
2023-07-12 19:58:43 +0900

    チャンネル一覧を取得するところまで動作確認完了

--- README.md
+++ README.md
@@ -1,5 +1,38 @@
-# チャンネルマップインフォ Bolt版
+# チャンネルマップインフォ bot Bolt版
+
 ## 概要
+
 - 毎日のチャンネルの参加者数増減を教えてくれる機能
 - times チャンネルのランキングを教えてくれる機能
-以上を提供するBoltで実装されたSlackBotです。
+  以上を提供するBoltで実装されたSlackBotです。
+
+内部的に https://api.slack.com/methods/conversations.list このAPIを使用しています。
+
+## 使い方
+
+Node.js v18.13.0 で動作確認。
+
+```
+npm install
+npm run build
+env SLACK_BOT_TOKEN=xxxxx SLACK_SIGNING_SECRET=xxxxx node dist/index.js
+```
+
+# Slack アプリケーションの作成方法
+
+https://qiita.com/seratch/items/1a460c08c3e245b56441 以上を参考に、WebSocket モードでアプリケーションを作成
+
+# Bot Token Scope の設定 (OAuth & Permissions)
+
+- chat:write
+- channels:read
+- groups:read
+- im:read
+- mpim:read
+
+# Event Subscription の設定 (Event Subscriptions)
+
+- message.channels
+- message.groups
+- message.im
+- message.mpim
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,61 @@
-console.log('hello world');
+import { App, LogLevel, GenericMessageEvent } from '@slack/bolt';
+
+const app = new App({
+  logLevel: LogLevel.INFO, // デバッグするときには DEBUG に変更
+  socketMode: true,
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  appToken: process.env.SLACK_APP_TOKEN,
+});
+
+// チャンネルマップインフォbotのコマンド操作の確認
+app.message(/^!ch-help/, async ({ message, say }) => {
+  const m = message as GenericMessageEvent;
+  await say(
+    `!ch-fetch 現在のチャンネル一覧の情報を所得する。すでに取得済みであれば取得しない。\n`,
+  );
+});
+
+// チャンネル一覧を取得するコマンド
+app.message(/^\!ch-fetch/, async ({ message, say }) => {
+  const m = message as GenericMessageEvent;
+  const channels = await fetchChannelList();
+  console.log(channels);
+  await say(
+    `<@${m.user}>さんの指示で、Slackからチャンネルリストを取得してファイル保存しました。`,
+  );
+});
+
+/**
+ * チャンネル一覧をSlackより取得し、cursorを使ったものをまとめて結合する
+ * @return Promise.<Object[]>
+ */
+async function fetchChannelList() {
+  let cursor;
+  let channels: any[] = [];
+
+  do {
+    const res = await app.client.conversations.list({
+      cursor: cursor,
+      exclude_archived: true,
+      limit: 1000,
+      types: 'public_channel',
+    });
+
+    if (res.ok && res.response_metadata) {
+      cursor = res.response_metadata.next_cursor;
+      channels = channels.concat(res.channels);
+    } else {
+      console.error(
+        '[ERROR] 正しくconversations.list APIが利用できませんでした。 res:',
+      );
+      console.error(res);
+    }
+  } while (cursor);
+
+  return { channels };
+}
+
+(async () => {
+  await app.start();
+})();
